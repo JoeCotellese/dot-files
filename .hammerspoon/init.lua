e545809ed9c4f8f84aee8e0fcea13c2c01c5bb66
Joe Cotellese <joec@aweber.com>
2015-12-10 16:24:59 -0500

    hammerspoon: initial commit

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,27 @@
+local hyper = {"ctrl", "alt", "cmd"}
+
+
+function hello()
+    hs.alert.show("Hello World!")
+end
+
+function full()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = max.x
+    f.y = max.y
+    f.w = max.w
+    f.h = max.h
+    win:setFrame(f)
+end
+
+hs.hotkey.bind(hyper, "g", full)
+
+-- lock the screen ala Windows NT
+hs.hotkey.bind(hyper,"delete", function()
+  hs.alert.show("startScreensaver")
+  hs.caffeinate.startScreensaver()
+end)
